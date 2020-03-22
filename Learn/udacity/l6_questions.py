870fec6ed925940282d79916588401594fea7ad1
Arcade Kwaku <arcade.kwaku@gmail.com>
2020-03-22 17:06:01 +0000

    update lesson 6

--- Learn/udacity/l6_questions.py
+++ Learn/udacity/l6_questions.py
@@ -18,3 +18,60 @@ def udacify2(name):
 
 print(udacify2('turn'))
 print(udacify2('boat'))
+
+
+""" Obfuscated Procedures
+Which of the following have exactly the 
+same behaviour as def proc.
+
+For two procedures to have exactly 
+the same behaviour, this means no matter what the inputs are
+the caller couldn't tell the diff between the procedures.
+
+In below 'test hasn't been defined. Assume test takes a number
+as it;s input and out puts a boolean.
+def proc(a,b):
+    if test(a):
+        return b
+    return a 
+"""
+
+
+
+""" 
+Define a procedure, median, that takes three
+ numbers as its inputs, and returns the median
+ of the three numbers.
+ Make sure your procedure has a return statement.
+
+"""
+
+def bigger(a,b):
+    if a > b:
+        return a
+    else:
+        return b
+
+def biggest(a,b,c):
+    return bigger(a,bigger(b,c))
+
+def median(a, b, c):
+    big = biggest(a,b,c)
+    if big == a:
+        return bigger(b,c)
+    if big == b:
+        return bigger(a,c)
+    else:
+        return bigger(a,b)
+     
+
+print(median(1,2,3))
+#>>> 2
+
+#print(median(9,3,6))
+#>>> 6
+
+#print(median(7,8,7))
+#>>> 7
+
+
